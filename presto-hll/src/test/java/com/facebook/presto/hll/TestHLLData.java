652563c221b1bd2dbe591857ab78441ce7366fa4
Miguel Ping <miguel-ping@telecom.pt>
2015-01-06 10:53:17 +0000

    add new test, move tests to proper package

---
+++ presto-hll/src/test/java/com/facebook/presto/hll/TestHLLData.java
@@ -0,0 +1,36 @@
+package com.facebook.presto.hll;
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import com.facebook.presto.hll.impl.HLL;
+import com.facebook.presto.hll.impl.RegisterSet;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestHLLData
+{
+        int[] v1 = new int[] {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,134217728,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
+        int[] v2 = new int[] {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,67108864,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
+
+    @Test
+    public void testSapoHLLEstimatorResults() throws Exception
+    {
+        int bitSize = 10;
+
+        HLL hll = new HLL(bitSize, new RegisterSet((int) Math.pow(2, bitSize), v2));
+        HLL merged = new HLL(bitSize, new RegisterSet((int) Math.pow(2, bitSize), v1));
+        merged = (HLL) merged.merge(hll);
+
+        Assert.assertEquals(merged.cardinality(), 2L);
+    }
+}
